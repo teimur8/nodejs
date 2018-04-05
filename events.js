cf1e3da9f8669fb9d8054d5e90831278858c87c5
My Name <yandemusic2017@yandex.ru>
2018-04-05 23:20:38 +0600

    lesson2

--- events.js
+++ events.js
@@ -1,17 +1,25 @@
 const { EventEmitter } = require('events');
+
 class Kettle extends EventEmitter{
+  constructor(){
+    super();
+    setImmediate(()=>{ // приоритет запуска 3
+      this.emit('created', {});
+    });
+  }
   start(){
-    setTimeout(()=>{
-      this.emit('ready', {});
-    },3000);
+    process.nextTick(() => { // приоритет 1
+      this.emit('start', {})
+    });
+    setTimeout(()=>{ this.emit('ready', {});},0); // приоритет 2
   }
 }
 
-
+console.log(123);
 
 const k  = new Kettle();
 k.start();
 
-k.on('ready', () => {
-  console.log('Чайник скипел');
-});
+k.on('created', () => {console.log('Чайник создан');});
+k.on('start', () => {console.log('Чайник начал');});
+k.on('ready', () => {console.log('Чайник скипел');});
