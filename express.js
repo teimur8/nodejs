b2d08743b7d3dd23fc276f69275423c02a6369e2
it06 <it06@kulanoil.kz>
2018-04-09 18:07:25 +0600

    ++

--- express.js
+++ express.js
@@ -4,17 +4,39 @@ const app = express();
 // middleware
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+// обтарботка мидла
+app.use((req, res, next) => {
+  req.blablabla = '123';
+  next();
+});
+
+
+app.all('/:id/:fName/:lName', (req, res, next) => {
+  // res.send(`id: ${req.params.id} fName: ${req.params.fName} lName:${req.params.lName} `);
+  res.send(JSON.stringify(req.params));
+  // next();
+});
+
+
 
 /*
 request and response
  */
+app.all('/', (req, res, next) => {
+  console.log('all');
+  next();
+});
 app.get('/', (req, res) => {
+  console.log(req.blablabla);
   res.send('Hello world');
 });
-
 app.post('/', (req,res) => {
   console.log(req.body);
   res.send('ok');
 });
+
+
+
+
 
 app.listen(8888);
