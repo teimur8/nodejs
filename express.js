bc1fad21341844dccee54af35b1b539c85b42806
it06 <it06@kulanoil.kz>
2018-04-10 14:15:01 +0600

    ++

--- express.js
+++ express.js
@@ -1,35 +1,42 @@
 const express = require('express');
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views",__dirname + "/views");
+
 // middleware
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 // обтарботка мидла
-app.use((req, res, next) => {
-  req.blablabla = '123';
-  next();
-});
+// app.use((req, res, next) => {
+//   req.blablabla = '123';
+//   next();
+// });
 
 
-app.all('/:id/:fName/:lName', (req, res, next) => {
-  // res.send(`id: ${req.params.id} fName: ${req.params.fName} lName:${req.params.lName} `);
-  res.send(JSON.stringify(req.params));
-  // next();
-});
-
-
+// app.all('/:id/:fName/:lName', (req, res, next) => {
+//   // res.send(`id: ${req.params.id} fName: ${req.params.fName} lName:${req.params.lName} `);
+//   res.send(JSON.stringify(req.params));
+//   // next();
+// });
 
 /*
 request and response
  */
-app.all('/', (req, res, next) => {
-  console.log('all');
-  next();
-});
 app.get('/', (req, res) => {
-  console.log(req.blablabla);
-  res.send('Hello world');
+  res.render("homepage", {
+    vegetables: [
+      "carrot",
+      "potato",
+      "beet"
+    ],
+    title:'Title'
+  });
 });
+// app.get('/', (req, res) => {
+//   console.log(req.blablabla);
+//   res.send('Hello world');
+// });
 app.post('/', (req,res) => {
   console.log(req.body);
   res.send('ok');
@@ -39,4 +46,4 @@ app.post('/', (req,res) => {
 
 
 
-app.listen(8888);
+app.listen(8889);
